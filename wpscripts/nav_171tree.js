7b10353836f68b98557db3029ce7bb99ad87efff
Kevin Evans <kevinevans@Kevins-Mac-mini.lan>
2019-08-04 16:34:42 +0100

    Upload

---
+++ wpscripts/nav_171tree.js
@@ -0,0 +1,26 @@
+var nav_171tree = WpNavBar.readTree({
+"childArray" : [
+{   'sTitle':'Home',
+    'bIsExternal':true,
+    'bIsWebPath':true,
+    'sUrl':'index.html'
+},
+{   'sTitle':'Community Council',
+    'bIsExternal':true,
+    'bIsWebPath':true,
+    'sUrl':'cccstart.html',
+    'sTarget':'_self'
+},
+{   'sTitle':'Community Archives',
+    'bIsExternal':true,
+    'bIsWebPath':true,
+    'sUrl':'ca.html',
+    'sTarget':'_self'
+},
+{   'sTitle':'Contact Us',
+    'bIsExternal':true,
+    'bIsWebPath':true,
+    'sUrl':'contact.html',
+    'sTarget':'_self'
+}]
+});
